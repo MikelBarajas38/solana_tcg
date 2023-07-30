4c5f33b6c5bdedacb8dbb5b65308ce69f3e8e0f3
yeicobF <jacobrdz34@gmail.com>
2023-07-30 06:23:19 -0600

    feat: add board background

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,32 @@ import type { AppProps } from "next/app"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "react-hot-toast"
+import Image from "next/image"
 
 const queryClient = new QueryClient()
+
+function BoardBackground() {
+  return (
+    <div className="fixed top-0 bottom-0 h-full w-full">
+      <Image
+        alt="Board"
+        src="/images/board-bg.png"
+        className="object-center object-cover h-full w-full"
+        quality={100}
+        fill={true}
+      />
+    </div>
+  )
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" reverseOrder={false} />
-
-      <Component {...pageProps} />
+      <BoardBackground />
+      <div className="z-30 relative h-screen">
+        <Component {...pageProps} />
+      </div>
     </QueryClientProvider>
   )
 }
